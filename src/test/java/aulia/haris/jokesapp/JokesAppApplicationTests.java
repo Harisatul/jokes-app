036a843a0681f7274e182d42904051dfeb20c56d
haris <haris.aulia@gmail.com>
2022-06-14 14:02:07 +0700

    initial commit

---
+++ src/test/java/aulia/haris/jokesapp/JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package aulia.haris.jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
